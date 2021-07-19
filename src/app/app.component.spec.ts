d723c240b47e5d600d11c3b601317f5358a00593
Kate Datema <k8datema@gmail.com>
2021-07-19 10:37:38 -0400

    uploading

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,24 +10,30 @@ import { NgForm } from '@angular/forms';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  //tells it where to go get the information
   providers:[ApiRedditService]
 })
+
+
 export class AppComponent {
+  // declaring reddit as a subreddit 
   reddit: SubReddit ={} as SubReddit
   title = 'RedditAPIApp';
 
-
+  //you want this to be private for sercuity? 
   constructor(private api: ApiRedditService){}
 
+  //declaring a method
 getPost(sub:string, slice:number){
-let response = this.api.getPosts(sub);
-console.log(response);
+  //getting the response from the main api
+  let response = this.api.getPosts(sub);
+  console.log(response);
 
-let output = response.subscribe(
-  (postlist: SubReddit) => this.reddit = 
-  {kind: postlist.kind, data: postlist.data});
+  //
+  let output = response.subscribe(
+   (postlist: SubReddit) => this.reddit = 
+    {kind: postlist.kind, data: postlist.data});
 
   output.splice(0, slice);
-
 }
 }
